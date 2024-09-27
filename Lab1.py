364dcc8f6e7241fcc0f4da082d663589859f410e
Christopher Porter <p2461598@katyisd.org>
2024-09-27 20:12:54 +0000

    make starter file and tests

---
+++ Lab1.py
@@ -0,0 +1,9 @@
+def peremiter(l,w):
+    return 0
+
+
+
+if __name__ == '__main__':
+    len = 1
+    wid = 2
+    print(peremiter(len,wid))
---
+++ TestLab1.py
@@ -0,0 +1,11 @@
+import unittest
+from Lab1 import peremiter
+class TestLab1(unittest.TestCase):
+
+    def test_ints(self):
+        self.assertEqual(peremiter(1,2),6)
+        self.assertEqual(peremiter(2,4),12)
+        
+
+if __name__ == '__main__':
+    unittest.main()
